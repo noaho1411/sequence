14aa7abdd82319299550b4df251ecb977652c3cf
noaho1411 <40387956+noaho1411@users.noreply.github.com>
2021-08-28 21:02:03 +1000

    Sequence 2.1
    added new features: 
    
    fixed scaling issues across mobile devices
    
    added emojis to the colored squares for colorblind accessibility
    
    increased brightness of square flashes
    
    added a current answer in emojis
    
    added reset current answer button
    
    added rules button
    
    added toggle button emojis button
    
    added functional difficulty settings
    
    added animation for start button border and correct or incorrect messages

--- script.js
+++ script.js
@@ -8,14 +8,63 @@ var answer="";
 var score = 0;
 // seperate counter which increases every 3 score (used for length increase) //
 var lcount=0;
+/* sets rules to invisible (if i do this in css, for some reason, the javascript
+doesn't realise that the display is off, so pressing the button for the first
+time sets the display to off again) */
+document.getElementById("ruletext").style.display = "none";
+/* initialising emojis as variables so they can be linked to each button (button 1 =
+emoji 1) for the answer tracker/ emoji toggle. e is added because variables cant start
+with an int */
+var e1 = "🍓"
+var e2 = "🍂"
+var e3 = "🌼"
+var e4 = "🍃"
+var e5 = "🌲"
+var e6 = "🦋"
+var e7 = "🌊"
+var e8 = "🍇"
+var e9 = "🌸"
+
+// automatically enable emojis //
+etoggle();
+
+function difficulty(){
+
+	var dif = document.getElementById("difficulty").value;
+	// assigns starting variables based on difficulty selection //
+	score=0
+	lcount=0
+	answer=""
+	if (dif === "easy"){
+		length=3;
+		delay=1500;
+	}
+	else if (dif === "hard"){
+		length=4;
+		delay=1250;
+	}
+	else{
+		length=6;
+		delay=1000;
+	}
+
+}
+function message(score,color){
+
+	document.getElementById("message").style.color=color;
+	document.getElementById("message").textContent=score;
+	document.getElementById("message").style.animation="answer 1.75s linear";
+
+	setTimeout(function() {
+		document.getElementById("message").style.animation="none";document.getElementById("message").style.color="black";
+	}, 1750)
+
+
+}
 
 function newsc(){
-	// resets all main variables 
-	length=3;
-	delay = 1500;
-	answer="";
-	score = 0;
-	lcount=0;
+	// changes difficulty at game start, and updates score (to 0 since its a new game)
+	difficulty();
 	document.getElementById("score").textContent = 'score: '+score;
 }
 
@@ -28,7 +77,10 @@ function win(){
 		length+=1;
 		// resets lcount so it will reach three in 3 more correct answers // 
 		lcount=0
-	}
+		}
+		// displays correct message //
+		message('Correct!','#95da3e');
+
 	// resets users answer, decreases the delay, so the game speeds up, and increases score by 1 //
 	answer="";
 	delay-=50;
@@ -43,28 +95,34 @@ function lose(){
 	delay = 1500;
 	score = 0;
 	l=0;
+	message('Incorrect :(','#b44343');
 	openm();
 }
 
 function main(color){
 	// takes input from button and adds it to answer string //
 	answer=answer+color;
+	// adds buttons emoji to the text in the current answer emojis string //
+	currentemojis = document.getElementById("ans");
+	currentemojis.textContent = currentemojis.textContent + eval("e"+(parseInt(color)+1))
+
 	// when the amount of button clickes matches the amount of answers, check the answer//
 	if (answer.length>=length){
 		check(answer);
+		// clears current emojis when round ends //	
+		setTimeout(function() {
+		currentemojis.textContent="";}, 1550);
 	}
 }
 
 function check(answer){
 	// if answer is correct, run sequences for winning, and generate a new answer/start a new round //
 	if (answer==window.sequence){
-		alert("correct");
 		win();
 		gen();
 	}
 	else{
 	// if it isnt, run sequence for losing //
-		alert("incorrect");
 		lose();
 	}
 }
@@ -111,6 +169,19 @@ function gen(){
 
 }
 
+function etoggle(){
+	// puts the corresponding emoji on each button if there are none, //
+	// takes them off if theyre already there //
+	for (var x of Array(9).keys()){
+	if (document.getElementById(x).textContent != ""){
+		document.getElementById(x).textContent = ""
+	}
+	else{
+		document.getElementById(x).textContent = eval("e"+(x+1))
+	}
+}
+}
+
 function wloop(sequence){
 	// runs the flash function with each button id as a parameter // 
 	for (let i=0; i < sequence.length; i++){
@@ -119,12 +190,28 @@ function wloop(sequence){
 		}
 	}
 
+function reset() {
+	// clears the users answer //
+	answer=""
+	document.getElementById("ans").textContent = "";
+
+}
+
+function rules() {
+	// if the ruletexts is visible, make it invisible, if it isn't, make it visible //
+	var ruletext = document.getElementById("ruletext");
+	if (ruletext.style.display === "none") {
+		ruletext.style.display = "block";
+	} else {
+		ruletext.style.display = "none";
+	}
+}
 
 
 
 //initialises elements of the pause menu as js variables //
 var modal = document.getElementById("myModal");
-var button = document.getElementById("myBtn");
+var button = document.getElementById("pause");
 var span = document.getElementsByClassName("close")[0];
 
 // the open modal function which is bound to buttons in the html now makes the modal visible //
@@ -136,4 +223,6 @@ function openm() {
 function closem() {
 	document.getElementById("myModal").style.display = "none";
 }
+
+
 
